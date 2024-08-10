0866d1471802999f54ac91f59d20b189612a63fe
Abhijeet Singh Rajput <abhijeet62008@gmail.com>
2024-08-10 17:13:36 +0530

    seperate the "google sign in" from "Login component", so that other coponent (Form) can use "google sign in" as well.

--- src/components/AddWarrior.js
+++ src/components/AddWarrior.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Login from './Login'
 import MultiStepForm from './form/MultiStepForm'
 import { useParams } from 'react-router-dom';
 
 export const AddWarrior = (warid) => {
-  const { war_id } = useParams(); 
+  const { war_id } = useParams();
+  const [isLogin, setIsLogin] = useState(
+    Boolean(localStorage.getItem('loginData'))
+  );
+
   return (
     <div className='w-[100%] h-[100%] overflow-y-auto'>
-     
-    <MultiStepForm war_id={war_id}/>
-    <Login/>
+      {isLogin && <MultiStepForm war_id={war_id} /> }
+      {!isLogin && <Login setIsLogin={setIsLogin} /> }
     </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,50 +1,16 @@
 import React from 'react';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { auth } from '../Firebase/firebaseConfig';
-import axios from 'axios';
-import { USER_REGISTER_LOGIN } from "../api/api_list";
 import GoogleButton from 'react-google-button';
 import { json } from 'react-router-dom';
+import {signin} from '../googleSignin';
 
-const Login = () => {
+const Login = ({setIsLogin}) => {
 
-    // Sign in with Google
-    const signin = async () => {
-        try {
-            const provider = new GoogleAuthProvider();
-            const result = await signInWithPopup(auth, provider);
-            const user = result.user;
-
-            // Filter important user information
-            const userInfo = {
-                name: user.displayName,
-                email: user.email,
-                uid: user.uid,
-                photoURL: user.photoURL
-            };
-
-            try {
-                // Send user information to your personal server
-                const response = await axios.post(USER_REGISTER_LOGIN, userInfo);
-                localStorage.setItem('loginData', JSON.stringify(response.data));
-                
-                // Optionally redirect or show user feedback
-                // e.g., window.location.href = '/home';
-            } catch (error) {
-                console.error('Error sending user info to server:', error);
-                // Handle error, show user notification, etc.
-            }
-        } catch (error) {
-            console.error('Error signing in with Google:', error);
-            // Handle error, show alert, etc.
-        }
-    }
 
     return (
         <div className="flex justify-center items-center">
             <GoogleButton
                 label='Continue with Google'
-                onClick={signin}
+                onClick={()=>signin(setIsLogin)}
             />
         </div>
     );
