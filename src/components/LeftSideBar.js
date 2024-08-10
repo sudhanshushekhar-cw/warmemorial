42b4d1bd3f476aa845def6f45cfebb2e08e6edf7
Abhijeet Singh Rajput <abhijeet62008@gmail.com>
2024-08-10 18:14:20 +0530

    fetch user profile photo in leftside bar

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../css/LeftSideBar.css';
 import { GET_SIDE_NAV_BAR } from "../api/api_list";
-import { Link } from 'react-router-dom';
+import { json, Link } from 'react-router-dom';
 import axios from 'axios';
 
 function Section({ heading, items }) {
@@ -58,6 +58,11 @@ export const LeftSideBar = ({ isSideBar }) => {
     'w-[70%] z-20 absolute md:w-[23%] md:relative' :
     'hidden z-10 md:w-[23%] md:block'
 
+  const loginData = localStorage.getItem('loginData');
+  let photo = '';
+  if(loginData){
+    photo = JSON.parse(loginData).photo;
+  }
   return (
     <>
       <div className={classes}>
@@ -67,7 +72,9 @@ export const LeftSideBar = ({ isSideBar }) => {
               <h3>War Memorial</h3>
               <p>Our India</p>
             </div>
-            <div className="img-box"></div>
+            <div className="img-box">
+              <img src={photo}></img>
+            </div>
           </header>
           <div id='section-wrapper' className='overflow-y-auto removeScrollBar'>
             <GetSideBar />
